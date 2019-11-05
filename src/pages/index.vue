fbc51447c0d286c8b5d8a46bd2e0fb646610bd30
Mazel <mazuel.loic@gmail.com>
2019-11-05 09:44:33 +0100

    (feature) Improve nuxt compatibilty

---
+++ src/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>
+      Component:
+    </h1>
+    <VueInputUi
+      id="input1"
+      v-model="value"
+      clearable
+    />
+  </div>
+</template>
+
+<script>
+  import VueInputUi from './../VueInputUi'
+  export default {
+    components: {
+      VueInputUi
+    },
+    data () {
+      return {
+        value: null
+      }
+    }
+  }
+</script>
